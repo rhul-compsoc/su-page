812cf70a1068f996872a93f765f385ada309fdad
James <jamesarnott2@gmail.com>
2022-11-08 23:00:59 +0000

    chore: files

---
+++ src/TestApp.tsx
@@ -0,0 +1,10 @@
+import '@/index.css';
+import Logo from '@/components/AnimatedLogo';
+
+const TestApp = () => (
+  <div className="w-screen h-screen">
+    <Logo />
+  </div>
+);
+
+export default TestApp;
