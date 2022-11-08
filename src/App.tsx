4e28ac6353aee6d4e27a69dae663053a9047e105
James <jamesarnott2@gmail.com>
2022-11-08 16:18:25 +0000

    fix: removed Link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import '@/index.css';
-import { Link } from 'react-router-dom';
 import { Section } from '@/components/Section';
 import { Buttons } from '@/components/Buttons';
 import { Button } from '@/components/Button';
